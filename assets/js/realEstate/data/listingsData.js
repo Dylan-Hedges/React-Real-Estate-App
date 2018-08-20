b1358b641a937d63b30983aad46e3b2cfd68e12d
DylanHedges <d.hedges1@hotmail.co.uk>
2018-08-20 08:25:35 -0400

    Added dummy house data

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,116 @@
+var listingsData = [
+  {
+    address: '97 Toronto Avenue',
+    city: 'Toronto',
+    state: 'ON',
+    rooms: 1,
+    price: 500000,
+    floorSpace: 700,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56587_11491_001.jpg'
+  },
+  {
+    address: '540 New York Road',
+    city: 'New York',
+    state: 'NY',
+    rooms: 1,
+    price: 620000,
+    floorSpace: 2000,
+    extras: [
+      'pool',
+      'gym'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56588_11491_002.jpg'
+  },
+  {
+    address: '82 San Francisco Road',
+    city: 'San Francisco',
+    state: 'CA',
+    rooms: 4,
+    price: 1220000,
+    floorSpace: 4300,
+    extras: [
+      'garden',
+      'pool'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56589_11491_003.jpg'
+  },
+  {
+    address: '102 Vancouver Avenue',
+    city: 'Vancouver',
+    state: 'BC',
+    rooms: 5,
+    price: 2220000,
+    floorSpace: 6000,
+    extras: [
+      'study',
+      'gym'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/220x165/56590_11491_004.jpg'
+  },
+  {
+    address: '12 Chicago Road',
+    city: 'Chicago',
+    state: 'IL',
+    rooms: 2,
+    price: 350000,
+    floorSpace: 1200,
+    extras: [
+      'garden',
+      'gym'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56587_11491_001.jpg'
+  },
+  {
+    address: '1234 Miami Street',
+    city: 'Miami',
+    state: 'FL',
+    rooms: 2,
+    price: 320000,
+    floorSpace: 1100,
+    extras: [
+      'pool',
+      'bar'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56588_11491_002.jpg'
+  },
+  {
+    address: '95 Texas Road',
+    city: 'Austin',
+    state: 'TX',
+    rooms: 6,
+    price: 1500000,
+    floorSpace: 5300,
+    extras: [
+      'gym',
+      'pool'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56589_11491_003.jpg'
+  },
+  {
+    address: '567 Arizona Avenue',
+    city: 'Pheonix',
+    state: 'AZ',
+    rooms: 6,
+    price: 7220000,
+    floorSpace: 6300,
+    extras: [
+      'garden',
+      'study'
+    ],
+    homeType: 'Appartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/220x165/56590_11491_004.jpg'
+  }
+]
+
+export default listingsData;
--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -1,6 +1,101 @@
 webpackJsonp([0],{
 
-/***/ 101:
+/***/ 100:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var listingsData = [{
+  address: '97 Toronto Avenue',
+  city: 'Toronto',
+  state: 'ON',
+  rooms: 1,
+  price: 500000,
+  floorSpace: 700,
+  extras: ['elevator', 'gym'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56587_11491_001.jpg'
+}, {
+  address: '540 New York Road',
+  city: 'New York',
+  state: 'NY',
+  rooms: 1,
+  price: 620000,
+  floorSpace: 2000,
+  extras: ['pool', 'gym'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56588_11491_002.jpg'
+}, {
+  address: '82 San Francisco Road',
+  city: 'San Francisco',
+  state: 'CA',
+  rooms: 4,
+  price: 1220000,
+  floorSpace: 4300,
+  extras: ['garden', 'pool'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56589_11491_003.jpg'
+}, {
+  address: '102 Vancouver Avenue',
+  city: 'Vancouver',
+  state: 'BC',
+  rooms: 5,
+  price: 2220000,
+  floorSpace: 6000,
+  extras: ['study', 'gym'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/220x165/56590_11491_004.jpg'
+}, {
+  address: '12 Chicago Road',
+  city: 'Chicago',
+  state: 'IL',
+  rooms: 2,
+  price: 350000,
+  floorSpace: 1200,
+  extras: ['garden', 'gym'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56587_11491_001.jpg'
+}, {
+  address: '1234 Miami Street',
+  city: 'Miami',
+  state: 'FL',
+  rooms: 2,
+  price: 320000,
+  floorSpace: 1100,
+  extras: ['pool', 'bar'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56588_11491_002.jpg'
+}, {
+  address: '95 Texas Road',
+  city: 'Austin',
+  state: 'TX',
+  rooms: 6,
+  price: 1500000,
+  floorSpace: 5300,
+  extras: ['gym', 'pool'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56589_11491_003.jpg'
+}, {
+  address: '567 Arizona Avenue',
+  city: 'Pheonix',
+  state: 'AZ',
+  rooms: 6,
+  price: 7220000,
+  floorSpace: 6300,
+  extras: ['garden', 'study'],
+  homeType: 'Appartment',
+  image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/220x165/56590_11491_004.jpg'
+}];
+
+exports.default = listingsData;
+
+/***/ }),
+
+/***/ 102:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -28,6 +123,10 @@ var _Listings = __webpack_require__(99);
 
 var _Listings2 = _interopRequireDefault(_Listings);
 
+var _listingsData = __webpack_require__(100);
+
+var _listingsData2 = _interopRequireDefault(_listingsData);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -44,12 +143,9 @@ var App = function (_Component) {
 
     var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
 
-    _this.clickedBtn = function () {
-      console.log('swag');
-    };
-
     _this.state = {
-      name: 'Joe'
+      name: 'Joe',
+      listingsData: _listingsData2.default
     };
     return _this;
   }
@@ -57,6 +153,7 @@ var App = function (_Component) {
   _createClass(App, [{
     key: 'render',
     value: function render() {
+      console.log(this.state.listingsData);
       return _react2.default.createElement(
         'div',
         null,
@@ -834,4 +931,4 @@ exports.default = Listings;
 
 /***/ })
 
-},[101]);
+},[102]);
