cc5bda23cebb7826d37b50b878c314e912a9914e
DylanHedges <d.hedges1@hotmail.co.uk>
2018-08-20 07:40:26 -0400

    Added styling for page navigation (pagination)

--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -782,10 +782,10 @@ var Listings = function (_Component) {
         ),
         _react2.default.createElement(
           'section',
-          { className: 'pagination' },
+          { id: 'pagination' },
           _react2.default.createElement(
             'ul',
-            { className: 'pagination-nums' },
+            { className: 'pages' },
             _react2.default.createElement(
               'li',
               null,
@@ -793,7 +793,7 @@ var Listings = function (_Component) {
             ),
             _react2.default.createElement(
               'li',
-              null,
+              { className: 'active' },
               '1'
             ),
             _react2.default.createElement(
